b7eae26f3ef967e3baa1ef2d9d052e52f3a0b538
ChiakiTakeuchi <takeuchi.chiako1025@gmail.com>
2025-01-23 13:42:30 +0900

    formでRegistForm、Viewでhome、Registを作成しました

--- forms.py
+++ forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from todo_project.todo.models import Task,
+
+class RegistForm(forms.ModelForm):
+    title = forms.CharField(label='タスク名')
+    description = forms.CharField(label='詳細説明')
+    expiry = forms.DateField(label='期限')
+
+    class Meta:
+        model = Task
+        fields = ['title','description','expiry']
 
 
-class RegistForm(forms.Form):
+
+
     
--- views.py
+++ views.py
@@ -1,12 +1,10 @@
+from .forms import RegistForm, 
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView,CreateView
 
 class HomeView(DetailView):
     template_name = 'home.html'
 
-
 class RegistUserView(CreateView):
     template_name = 'regist.html'
     form_class = RegistForm
-
-    
